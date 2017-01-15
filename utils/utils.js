496b19474e5046806bc45010752eeb02947e864d
qianchen <qianchen245@gmail.com>
2017-01-15 11:23:34 +0800

    pm2Scripts

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,6 @@
 exports.mapProp = (dic, name) => {
 	let _newProp = ''
 	for (let prop in dic) {
-		console.info(prop, name)
 		if (dic[prop] === name || dic[prop].indexOf(name) >= 0) {
 			_newProp = prop
 
